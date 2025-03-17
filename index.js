5cdf18a46da80a9066d941143eef84cc8e3aa85e
David Bruant <davidbruant@protonmail.com>
2025-03-17 12:06:10 +0100

    téléchargement de toutes les images

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+import {join, extname} from 'node:path';
+import {mkdir} from 'node:fs/promises'
+import {createWriteStream} from 'node:fs'
+
+import https from 'node:https'
+
+
 import minimist from 'minimist'
 import { createRestAPIClient } from 'masto';
+
 
 const accessToken = process.env.TOKEN
 if(!accessToken){
@@ -13,8 +21,8 @@ console.log(argv);
 const mastoAccount = argv['_'][0]
 
 const pieces = mastoAccount.match(/^@(.*)@(.*)$/)
+//console.log('pieces', pieces)
 
-console.log('pieces', pieces)
 const [_, accountName, accountHostname] = pieces
 
 const masto = await createRestAPIClient({
@@ -27,12 +35,71 @@ const accountId = account.id;
 
 console.log('accountName', accountName, accountId)
 
+const IMAGES_SUBDIR_NAME = 'images'
+
+const backupDirName = `backup-${mastoAccount}`
+const backupDirPath = join(process.cwd(), backupDirName)
+const backupImagesDirPath = join(backupDirPath, IMAGES_SUBDIR_NAME)
+
+try{
+    await mkdir(backupDirPath)
+    await mkdir(backupImagesDirPath)
+}
+catch(e){
+    if(e.code === 'EEXIST'){
+        //ignore
+    }
+    else{
+        throw e
+    }
+}
+
+function downloadImage(url, filepath){
+    console.log('download', url, 'to', filepath)
+
+    return new Promise((resolve, reject) => {
+        const fileWriteStream = createWriteStream(filepath)
+        const resp = https.get(url, res => {
+            res.pipe(fileWriteStream)
+        
+            res.on('error', reject)
+
+        })
+        
+        fileWriteStream.on('error', reject)
+        fileWriteStream.on('finish', resolve)
+    })
+}
+
+
 const statusPaginator = masto.v1.accounts.$select(accountId).statuses.list({limit: 40, onlyMedia: true})
 
-const page = await statusPaginator.values()
+let imagePathToAltText = new Map()
 
-const simplifiedPage = page.map(({id, createdAt, content, mediaAttachments, language}) => ({id, createdAt, content, mediaAttachments, language}))
+for await (const page of statusPaginator){
+    //console.log('page', page.length, page[0].id)
 
-console.log('simplifiedPage', simplifiedPage, simplifiedPage.length)
+    for(const status of page){
+        //const {id, createdAt, content, mediaAttachments, language} = page
+        const {mediaAttachments} = status
 
-console.log('medias', simplifiedPage.map(p => p.mediaAttachments))
+        for(const mediaAttachement of mediaAttachments){
+            const {id, url, type, description} = mediaAttachement
+
+            if(type === 'image' && url){
+                const extension = extname(url)
+                const filename = `${id}${extension}`
+                //console.log('filename', filename, url)
+                imagePathToAltText.set(`${IMAGES_SUBDIR_NAME}/${filename}`, description)
+
+                const pathname = join(backupImagesDirPath, filename)
+                await downloadImage(url, pathname)
+            }
+            else{
+                // ignore
+            }
+        }
+    }
+
+    
+}
